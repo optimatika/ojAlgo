4470f58b38355cad1383f18158d9c606dfb11885
apete <apete@optimatika.se>
2017-07-17 00:34:36 +0200

    Stopwatch

---
+++ src/org/ojalgo/type/Stopwatch.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 1997-2017 Optimatika (www.optimatika.se)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.ojalgo.type;
+
+import java.time.Duration;
+
+/**
+ * This stopwatch is always running. It start as soon as you create the instance.
+ *
+ * @author apete
+ */
+public class Stopwatch {
+
+    /**
+     * Meassure task duration using this class' stopwatch functionality.
+     *
+     * @param task The task to meassure
+     * @return The meassured duration
+     */
+    public static Duration meassure(final Runnable task) {
+
+        final Stopwatch timer = new Stopwatch();
+
+        task.run();
+
+        return timer.stop();
+    }
+
+    private long myStart;
+    private long myStop;
+
+    public Stopwatch() {
+
+        super();
+
+        this.reset();
+    }
+
+    /**
+     * Reset the start-instant
+     */
+    public void reset() {
+        myStart = System.nanoTime();
+        myStop = myStart;
+    }
+
+    /**
+     * This method can be called repeatedly without resetting
+     *
+     * @return The duration since instantiation or reset.
+     */
+    public Duration stop() {
+        myStop = System.nanoTime();
+        return Duration.ofNanos(myStop - myStart);
+    }
+
+}
