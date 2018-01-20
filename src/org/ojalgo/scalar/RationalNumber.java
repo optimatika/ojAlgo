1c5cd09f31d7d053ba7aab6515b1c1176137c309
apete <apete@optimatika.se>
2018-01-21 00:11:25 +0100

    RationalNumber

--- src/org/ojalgo/scalar/RationalNumber.java
+++ src/org/ojalgo/scalar/RationalNumber.java
@@ -178,8 +178,6 @@ public final class RationalNumber extends Number implements Scalar<RationalNumbe
             return NEGATIVE_INFINITY;
         }
 
-        return RationalNumber.valueOf(BigDecimal.valueOf(value));
-
         //        final long valBits = Double.doubleToLongBits(value);
         //
         //        final int sign = ((valBits >> 63) == 0 ? 1 : -1);
@@ -192,6 +190,38 @@ public final class RationalNumber extends Number implements Scalar<RationalNumbe
         //        } else {
         //            return RationalNumber.of(sign * significand * (1L << exponent), 1L);
         //        }
+
+        // Code below doesn't work for 0 and NaN - just check before
+
+        //        final long bits = Double.doubleToLongBits(value);
+        //
+        //        final long sign = bits >>> 63;
+        //        final long exponent = ((bits >>> 52) ^ (sign << 11)) - 1023;
+        //        final long fraction = bits << 12; // bits are "reversed" but that's not a problem
+        //
+        //        long a = 1L;
+        //        long b = 1L;
+        //
+        //        for (int i = 63; i >= 12; i--) {
+        //            a = (a * 2) + ((fraction >>> i) & 1);
+        //            b *= 2;
+        //        }
+        //
+        //        if (exponent > 0) {
+        //            a *= 1 << exponent;
+        //        } else {
+        //            b *= 1 << -exponent;
+        //        }
+        //
+        //        if (sign == 1) {
+        //            a *= -1;
+        //        }
+        //
+        //        // Here you have to simplify the fraction
+        //
+        //        System.out.println(a + "/" + b);
+
+        return RationalNumber.valueOf(BigDecimal.valueOf(value));
     }
 
     public static RationalNumber valueOf(final Number number) {
