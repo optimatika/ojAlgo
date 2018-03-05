8eee5c077c855b14ac673ffd07cb7a26abab2a30
apete <apete@optimatika.se>
2018-03-05 22:03:06 +0100

    Copyright 1997-2018 Optimatika

--- src/org/ojalgo/random/Poisson.java
+++ src/org/ojalgo/random/Poisson.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1997-2017 Optimatika
+ * Copyright 1997-2018 Optimatika
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- test/org/ojalgo/optimisation/integer/MipCase.java
+++ test/org/ojalgo/optimisation/integer/MipCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1997-2017 Optimatika Permission is hereby granted, free of charge, to any person
+ * Copyright 1997-2018 Optimatika Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software
  * without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
