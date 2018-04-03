0d0917f513251ad83b7fc2890a0815db190239b4
apete <apete@optimatika.se>
2018-04-03 18:30:42 +0200

    Moved BenchmarkResultsParser

--- src/org/ojalgo/netio/TableData.java
+++ src/org/ojalgo/netio/TableData.java
@@ -9,48 +9,58 @@ import org.ojalgo.access.Structure1D.IndexMapper;
 import org.ojalgo.array.LongToNumberMap;
 import org.ojalgo.array.LongToNumberMap.MapFactory;
 import org.ojalgo.array.Primitive64Array;
+import org.ojalgo.type.context.NumberContext;
 
 public final class TableData<R> {
 
+    private static final NumberContext GENERAL = NumberContext.getGeneral(8);
+
     private static final MapFactory<Double> COLUMN_FACTORY = LongToNumberMap.factory(Primitive64Array.FACTORY);
 
-    private Map<String, LongToNumberMap<Double>> myColumns = new HashMap<>();
-    private Set<R> myRows = new TreeSet<>();
-    private IndexMapper<R> myRowIndexMapper;
+    private final Map<String, LongToNumberMap<Double>> myColumns = new HashMap<>();
+    private final IndexMapper<R> myRowIndexMapper;
+    private final Set<R> myRows = new TreeSet<>();
 
-    public TableData(IndexMapper<R> rowIndexMapper) {
+    public TableData(final IndexMapper<R> rowIndexMapper) {
         super();
         myRowIndexMapper = rowIndexMapper;
     }
 
-    public double doubleValue(R row, String col) {
+    public double doubleValue(final R row, final String col) {
         return myColumns.computeIfAbsent(col, (c) -> COLUMN_FACTORY.make()).doubleValue(myRowIndexMapper.toIndex(row));
     }
 
     public CharSequence print() {
 
-        StringBuilder builder = new StringBuilder();
+        final StringBuilder builder = new StringBuilder();
 
-        Set<String> columnKeys = myColumns.keySet();
+        final Set<String> columnKeys = myColumns.keySet();
 
         builder.append("Dimension");
-        for (String col : columnKeys) {
+        for (final String col : columnKeys) {
             builder.append(";");
             builder.append(col);
         }
 
-        for (R row : myRows) {
+        builder.append(LineTerminator.UNIX);
+
+        for (final R row : myRows) {
             builder.append(row);
-            for (String col : columnKeys) {
+            for (final String col : columnKeys) {
                 builder.append(";");
-                builder.append(myColumns.get(col).doubleValue(myRowIndexMapper.toIndex(row)));
+                final double tmpDoubleValue = myColumns.get(col).doubleValue(myRowIndexMapper.toIndex(row));
+                if (!Double.isNaN(tmpDoubleValue)) {
+
+                    builder.append(GENERAL.format(tmpDoubleValue));
+                }
             }
+            builder.append(LineTerminator.UNIX);
         }
 
         return builder;
     }
 
-    public void put(R row, String col, double value) {
+    public void put(final R row, final String col, final double value) {
         myRows.add(row);
         myColumns.computeIfAbsent(col, (c) -> COLUMN_FACTORY.make()).put(myRowIndexMapper.toIndex(row), value);
     }
