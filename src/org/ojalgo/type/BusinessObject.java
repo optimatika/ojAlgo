99cf634371f1db2001f56436f647a0eaf20fac0f
apete <apete@optimatika.se>
2018-07-18 19:25:11 +0200

    Removed org.ojalgo.business

--- src/org/ojalgo/type/BusinessObject.java
+++ src/org/ojalgo/type/BusinessObject.java
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package org.ojalgo.business;
+package org.ojalgo.type;
 
 import java.util.Collections;
 import java.util.List;
