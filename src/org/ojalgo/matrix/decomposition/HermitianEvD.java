c2b6113c32a77d505415184830d2ea0cc2cfbb00
apete <apete@optimatika.se>
2018-10-08 22:11:44 +0200

    l+1 -> m

--- src/org/ojalgo/matrix/decomposition/HermitianEvD.java
+++ src/org/ojalgo/matrix/decomposition/HermitianEvD.java
@@ -131,7 +131,7 @@ public abstract class HermitianEvD<N extends Number> extends EigenvalueDecomposi
             }
 
             // If m == l, d[l] is an eigenvalue, otherwise, iterate.
-            if ((m > l) && ((l + 1) < size)) {
+            if ((m > l) && (m < size)) {
                 do {
 
                     // Compute implicit shift
