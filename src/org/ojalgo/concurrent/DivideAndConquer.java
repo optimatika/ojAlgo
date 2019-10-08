7d99b38c3467199f921b11c768241ee3b13d26c0
apete <apete@optimatika.se>
2019-10-08 20:23:11 +0200

    Revert "DivideAndConquer"
    This reverts commit 53ace4e3c7e043c13516a807d850674ec9ae77f4.

--- src/org/ojalgo/concurrent/DivideAndConquer.java
+++ src/org/ojalgo/concurrent/DivideAndConquer.java
@@ -44,19 +44,14 @@ public abstract class DivideAndConquer extends Object {
      */
     public final void invoke(final int first, final int limit, final int threshold) {
 
-        final int availableWorkers = OjAlgoUtils.ENVIRONMENT.threads - DaemonPoolExecutor.INSTANCE.getActiveCount();
+        final int availableWorkers = OjAlgoUtils.ENVIRONMENT.threads - (DaemonPoolExecutor.INSTANCE.getActiveCount() / 2);
 
-        try {
-            this.divide(first, limit, threshold, availableWorkers).get();
-        } catch (InterruptedException | ExecutionException exception) {
-            exception.printStackTrace();
-            throw new ProgrammingError(exception);
-        }
+        this.divide(first, limit, threshold, availableWorkers);
     }
 
     protected abstract void conquer(final int first, final int limit);
 
-    final Future<?> divide(final int first, final int limit, final int threshold, final int workers) {
+    final void divide(final int first, final int limit, final int threshold, final int workers) {
 
         final int count = limit - first;
 
@@ -65,21 +60,20 @@ public abstract class DivideAndConquer extends Object {
             final int split = first + (count / 2);
             final int nextWorkers = workers / 2;
 
-            final Future<?> firstPart = this.divide(first, split, threshold, nextWorkers);
-            final Future<?> secondPart = this.divide(split, limit, threshold, nextWorkers);
+            final Future<?> firstPart = DaemonPoolExecutor.INSTANCE.submit(() -> this.divide(first, split, threshold, nextWorkers));
+            final Future<?> secondPart = DaemonPoolExecutor.INSTANCE.submit(() -> this.divide(split, limit, threshold, nextWorkers));
 
             try {
                 firstPart.get();
+                secondPart.get();
             } catch (final InterruptedException | ExecutionException exception) {
                 exception.printStackTrace();
                 throw new ProgrammingError(exception);
             }
 
-            return secondPart;
-
         } else {
 
-            return DaemonPoolExecutor.INSTANCE.submit(() -> this.conquer(first, limit));
+            this.conquer(first, limit);
         }
     }
 
