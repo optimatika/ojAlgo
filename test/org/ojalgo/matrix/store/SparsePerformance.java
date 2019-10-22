72a7e8b043b2b496dc0bef35b0116cadbba45755
apete <apete@optimatika.se>
2019-10-22 22:49:44 +0200

    Update SparsePerformance.java

--- test/org/ojalgo/matrix/store/SparsePerformance.java
+++ test/org/ojalgo/matrix/store/SparsePerformance.java
@@ -76,7 +76,7 @@ public class SparsePerformance extends MatrixStoreTests {
 
         SparsePerformance.touchNonzeros(mtrxA);
 
-        TestUtils.assertFasterThan(9, CalendarDateUnit.MILLIS, clock);
+        TestUtils.assertFasterThan(10, CalendarDateUnit.MILLIS, clock);
     }
 
     @Test
