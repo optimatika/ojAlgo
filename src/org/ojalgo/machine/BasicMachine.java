c93fc943e8b2deb7b6c485b894e04f8d43d01a43
apete <apete@optimatika.se>
2020-01-08 19:44:10 +0100

    AbstractMachine

--- src/org/ojalgo/machine/BasicMachine.java
+++ src/org/ojalgo/machine/BasicMachine.java
@@ -66,7 +66,7 @@ public class BasicMachine {
     public String toString() {
 
         int tmpPrefix = 1;
-        int tmpMeasure = (int) (memory / AbstractMachine.K);
+        int tmpMeasure = (int) (memory / CommonMachine.K);
 
         while ((tmpMeasure / 1024) > 0) {
             tmpPrefix++;
