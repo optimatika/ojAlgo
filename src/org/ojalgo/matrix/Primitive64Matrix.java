45f7afe4d42baef30084ea7b286fc36e4ae9e204
xwaeaewcrhomesysplug <67897598+xwaeaewcrhomesysplug@users.noreply.github.com>
2020-09-12 12:31:53 +0900

    Update Primitive64Matrix.java

--- src/org/ojalgo/matrix/Primitive64Matrix.java
+++ src/org/ojalgo/matrix/Primitive64Matrix.java
@@ -41,6 +41,8 @@ import org.ojalgo.structure.Structure2D;
 
 /**
  * PrimitiveMatrix
+ *<pre>
+ *64 means it's primitive float as element,as of v48.3.0 by xwaeaewcrhomesysplug.</pre>
  *
  * @author apete
  */
