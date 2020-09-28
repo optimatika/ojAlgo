cc9c63574a41a3d9f4aeb8d27ce93787eb8e118d
Magnus <mjansson@thermocalc.se>
2020-09-28 11:35:09 +0200

    Add test where equality are converted into two inequalities

--- test/org/ojalgo/optimisation/convex/LagrangeTest.java
+++ test/org/ojalgo/optimisation/convex/LagrangeTest.java
@@ -151,6 +151,30 @@ public class LagrangeTest extends OptimisationConvexTests {
         Optional<Access1D<?>> multipliers = result.getMultipliers();
         TestUtils.assertTrue("No multipliers present", multipliers.isPresent());
         TestUtils.assertEquals("Lagrangian Multipliers differ", expectedDual, multipliers.get(), accuracy);
+        
+//  Test similar system where each equality constraint are converted into two inequality constraints.
+//  The result should be the same.
+       Builder ieBuilder = ConvexSolver.getBuilder();
+       ieBuilder.objective(Q, C.negate());
+       MatrixStore<Double> AI = AE.logical().below(AE.negate()).get();
+       MatrixStore<Double> BI = BE.logical().below(BE.negate()).get();
+       ieBuilder.inequalities(AI, BI);
+       ConvexSolver ieSolver = ieBuilder.build();
+       
+       Result ieResult = ieSolver.solve();
+       Optional<Access1D<?>> ieMultipliers = ieResult.getMultipliers();
+       TestUtils.assertTrue("No multipliers present", ieMultipliers.isPresent());
+
+       int greaterThanCount = 0;
+       int greaterThanOrEqualCount = 0;
+       for (double v : ieMultipliers.get().toRawCopy1D()) {
+          if (v == 0)greaterThanOrEqualCount++;
+          if (v > 0) greaterThanCount++;
+       }
+// Test that two multipliers are greater than zero and two are zero.
+       TestUtils.assertTrue(greaterThanOrEqualCount == 2);
+       TestUtils.assertTrue(greaterThanCount == 2);
+       
     }
 
 }
