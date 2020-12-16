e03891f364a1c51ef8371361b1a9d059c174188b
Anders Peterson <apete@users.noreply.github.com>
2020-12-16 16:24:54 +0100

    Changed presolver signature (#313)

--- src/org/ojalgo/optimisation/ExpressionsBasedModel.java
+++ src/org/ojalgo/optimisation/ExpressionsBasedModel.java
@@ -343,10 +343,12 @@ public final class ExpressionsBasedModel extends AbstractModel {
          * @param remaining TODO
          * @param lower TODO
          * @param upper TODO
+         * @param relaxed TODO
          * @return True if any model entity was modified so that a re-run of the presolvers is necessary -
          *         typically when/if a variable was fixed.
          */
-        public abstract boolean simplify(Expression expression, Set<IntIndex> remaining, BigDecimal lower, BigDecimal upper, NumberContext precision);
+        public abstract boolean simplify(Expression expression, Set<IntIndex> remaining, BigDecimal lower, BigDecimal upper, NumberContext precision,
+                boolean relaxed);
 
         @Override
         boolean isApplicable(final Expression target) {
@@ -1360,12 +1362,12 @@ public final class ExpressionsBasedModel extends AbstractModel {
             BigDecimal upper = tmpExpr.getUpperLimit();
 
             if (tmpExpr.isObjective()) {
-                Presolvers.LINEAR_OBJECTIVE.simplify(tmpExpr, allVars, lower, upper, options.feasibility);
+                Presolvers.LINEAR_OBJECTIVE.simplify(tmpExpr, allVars, lower, upper, options.feasibility, !anyVarInt);
             }
             if (tmpExpr.isConstraint()) {
-                Presolvers.ZERO_ONE_TWO.simplify(tmpExpr, allVars, lower, upper, options.feasibility);
+                Presolvers.ZERO_ONE_TWO.simplify(tmpExpr, allVars, lower, upper, options.feasibility, !anyVarInt);
                 if (anyVarInt) {
-                    Presolvers.INTEGER_EXPRESSION_ROUNDING.simplify(tmpExpr, allVars, lower, upper, options.feasibility);
+                    Presolvers.INTEGER_EXPRESSION_ROUNDING.simplify(tmpExpr, allVars, lower, upper, options.feasibility, !anyVarInt);
                 }
             }
         }
@@ -1525,7 +1527,7 @@ public final class ExpressionsBasedModel extends AbstractModel {
 
                     for (final Presolver presolver : PRESOLVERS) {
                         if (!needToRepeat) {
-                            needToRepeat |= presolver.simplify(expr, myTemporary, compensatedLowerLimit, compensatedUpperLimit, options.feasibility);
+                            needToRepeat |= presolver.simplify(expr, myTemporary, compensatedLowerLimit, compensatedUpperLimit, options.feasibility, myRelaxed);
                         }
                     }
                 }
