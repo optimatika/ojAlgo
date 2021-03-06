a7f01086728782c80dd28c8aabe9be79f90cfd44
apete <apete@optimatika.se>
2021-03-06 23:36:12 +0100

    Copyright 2021

--- src/main/java/org/ojalgo/tensor/AnyTensor.java
+++ src/main/java/org/ojalgo/tensor/AnyTensor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1997-2020 Optimatika
+ * Copyright 1997-2021 Optimatika
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
