54871d51749be3c7f0437b6c07a87388c396f138
apete <apete@optimatika.se>
2022-01-02 20:30:45 +0100

    Copyright 1997-2022 Optimatika

--- src/test/java/org/ojalgo/optimisation/convex/P20150809.java
+++ src/test/java/org/ojalgo/optimisation/convex/P20150809.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1997-2021 Optimatika
+ * Copyright 1997-2022 Optimatika
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
