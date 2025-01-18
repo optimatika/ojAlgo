8f4dd2ee1335b9812c1bf069adb7dd89f6ab3142
apete <apete@optimatika.se>
2025-01-18 19:08:41 +0100

    Copyright 1997-2025

--- src/test/java/org/ojalgo/optimisation/integer/RelaxedMIPCase.java
+++ src/test/java/org/ojalgo/optimisation/integer/RelaxedMIPCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1997-2024 Optimatika
+ * Copyright 1997-2025 Optimatika
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software", to deal
